4837068db559b3683fc9766e2f02b20d928bd102
kevindwiwijaya <87982064+kevindwiwijaya@users.noreply.github.com>
2021-12-07 13:47:08 +0700

    Add files via upload

---
+++ webjersey/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Kios Jersey | DODO</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <div class="header">
+    <p>Kios Jersey | DODO</p>
+  </div>
+  <nav>
+    <ul> <?php include("page/navbar.php") ?> </ul>
+  </nav>
+
+  <?php include("content.php") ?>
+
+  <?php include("page/user/keranjang.php") ?>
+  <div class="footer">
+    
+  </div>
+
+</body>
+
+</html>
